2bbac90041ef96add62b67366354cb92671ef34a
kenfutamata <104425231+kenfutamata@users.noreply.github.com>
2024-03-07 09:33:46 +0800

    Add files via upload

---
+++ consolepracticalfinal/consolepracticalfinal/Program.cs
@@ -0,0 +1,25 @@
+﻿using consolepracticalfinal;
+using System; 
+
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        int size;
+        Console.Write("Enter the size of an array: ");
+        size = Convert.ToInt32(Console.ReadLine()); 
+        int[] arr = new int[size];
+        Console.WriteLine("Enter the " + size + " elements of an array: "); 
+        for(int i = 0; i < size; i++)
+        {
+            arr[i] = Convert.ToInt32(Console.ReadLine()); 
+        }
+
+        encap es = new encap(arr);
+        es.display();
+        Console.WriteLine();
+        es.odddisplay(); 
+        Console.WriteLine();
+        es.evendisplay(); 
+    }
+}
---
+++ consolepracticalfinal/consolepracticalfinal/encap.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consolepracticalfinal
+{
+    public class encap
+    {
+        private int[] arr;
+        public encap(int[] arr)
+        {
+            this.arr = arr; 
+        }
+
+        public int[] getArr()
+        {
+            return arr; 
+        }
+
+        public void display()
+        {
+            Console.Write("Array Elements: "); 
+            foreach(int i in getArr())
+            {
+                Console.Write(" "+i);
+            }
+        }
+
+        public void odddisplay()
+        {
+            Console.Write("Odd Array Elements: "); 
+            for(int i = 0; i < getArr().Length; i++)
+            {
+                if (arr[i] % 2 == 0)
+                {
+                    Console.Write(" " + i); 
+                }
+            }
+            int sum = 0;
+            Console.WriteLine(); 
+            Console.Write("Odd Array Sum: ");
+            for (int i = 0; i < getArr().Length; i++)
+            {
+                if (arr[i] % 2 == 0)
+                {
+                    sum += i;
+                    
+                }
+            }
+            Console.Write(" " + sum);
+        }
+
+        public void evendisplay()
+        {
+            Console.Write("Even Array Elements: ");
+            for (int i = 0; i < getArr().Length; i++)
+            {
+                if (arr[i] % 2 != 0)
+                {
+                    Console.Write(" " + i);
+                }
+            }
+            int sum = 0;
+            Console.WriteLine();
+            Console.Write("Even Array Sum: ");
+            for (int i = 0; i < getArr().Length; i++)
+            {
+                if (arr[i] % 2 != 0)
+                {
+                    sum += i;
+                    
+                }
+            }
+            Console.Write(" " + sum);
+        }
+    }
+}
